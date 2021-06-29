5d704146ea8a32db47734031d0f2a9eaf7a0ae5c
Luke <mayom.luke@gmail.com>
2021-06-28 19:26:51 -0500

    Coroutine Example

---
+++ Assets/CoroutineExample.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutineExample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(UntilRoutine());
+    }
+
+    bool CheckInput()
+    {
+        return Input.GetKeyDown(KeyCode.Space);
+    }
+
+    IEnumerator UntilRoutine()
+    {
+        print("Start of until");
+
+        yield return new WaitUntil(CheckInput);
+
+        print("you pressed something");
+    }
+
+    IEnumerator ExampleRoutine()
+    {
+        print("Start of the Coroutine");
+
+        float time = 5;
+        while (time >= 0)
+        {
+            time -= Time.deltaTime;
+            yield return null;
+        }
+
+        print("End of the Coroutine");
+    }
+}
